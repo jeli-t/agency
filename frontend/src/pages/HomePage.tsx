c8c966d5acfde784c17e646f00d0ef98c27cbc55
jeli-t <jelitotomek@gmail.com>
2023-06-09 21:34:44 +0200

    add writing animation for HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,30 @@
-import { useMantineTheme, MantineProvider, Text } from '@mantine/core';
-
+import { useMantineTheme, MantineProvider, Text, Center } from '@mantine/core';
+import { TypeAnimation } from 'react-type-animation';
 
 export default function HomePage() {
     const mainTheme = useMantineTheme();
 
     return (
         <MantineProvider withGlobalStyles withNormalizeCSS>
-            <Text>Welcome to jeli.pl!</Text>
+            <Center h={'100vh'}>
+                <TypeAnimation
+                    sequence={[
+                        'jeli.pl',
+                        2000,
+                        'Web design',
+                        1000,
+                        'Web development',
+                        1000,
+                        'Search Engine Optimization',
+                        1000,
+                        'Project management',
+                        1000,
+                        'jeli.pl',
+                    ]}
+                    wrapper="span"
+                    style={{ fontSize: '3em', display: 'inline-block' }}
+                />
+            </Center>
         </MantineProvider>
     )
 }
