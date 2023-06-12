1996f55b7ba470ddf28c4ecbbadd1515fa99b729
jeli-t <jelitotomek@gmail.com>
2023-06-12 21:01:56 +0200

    add UnderConstructionPage

---
+++ frontend/src/pages/UnderConstructionPage.tsx
@@ -0,0 +1,51 @@
+import { createStyles, Title, Text, Button, rem, MantineProvider, useMantineTheme, Image } from '@mantine/core';
+import construction from './../assets/construction.svg'
+
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+
+  title: {
+    fontFamily: `Roboto, ${theme.fontFamily}`,
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: rem(60),
+    marginTop: 50,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: rem(32),
+    },
+  },
+
+  description: {
+    maxWidth: rem(500),
+    marginTop: theme.spacing.xl,
+  },
+}));
+
+export default function UnderConstructionPage() {
+  const { classes } = useStyles();
+  const mainTheme = useMantineTheme();
+
+  return (
+    <MantineProvider theme={mainTheme} withGlobalStyles withNormalizeCSS>
+      <div className={classes.root}>
+        <Title className={classes.title}>Construction site</Title>
+        <Text size="lg" align="center" className={classes.description}>
+          This site is still under construction. Good things take time. Come back later!
+        </Text>
+        <Image src={construction} height={'500px'} width='auto' />
+        <a href='/'>
+          <Button color='orange' variant="light" size="md">
+            Take me back to home page
+          </Button>
+        </a>
+      </div>
+    </MantineProvider>
+  );
+}
