b0d02f61601252963abca86320e462ea8154f6d3
jeli-t <jelitotomek@gmail.com>
2023-07-11 13:50:40 +0200

    add content for FaqSection

--- frontend/src/components/FaqSection.tsx
+++ frontend/src/components/FaqSection.tsx
@@ -34,28 +34,48 @@ export function FaqSection() {
 
       <Accordion variant="separated">
         <Accordion.Item className={classes.item} value="reset-password">
-          <Accordion.Control>How can I reset my password?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
+          <Accordion.Control>
+            Is your company registered?
+          </Accordion.Control>
+          <Accordion.Panel>
+            Not yet but I'am working on that. For now I work as a freelancer based on a contract for work.
+          </Accordion.Panel>
         </Accordion.Item>
 
         <Accordion.Item className={classes.item} value="another-account">
-          <Accordion.Control>Can I create more that one account?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
+          <Accordion.Control>
+            What technologies do you use?
+          </Accordion.Control>
+          <Accordion.Panel>
+            We create most of the websites using Wordpress Elementor, so anyone can easily update information on the site without hiring a specialist. For more complex projects, we use React JS and Django.
+          </Accordion.Panel>
         </Accordion.Item>
 
         <Accordion.Item className={classes.item} value="newsletter">
-          <Accordion.Control>How can I subscribe to monthly newsletter?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
+          <Accordion.Control>
+            What experience do you have?
+          </Accordion.Control>
+          <Accordion.Panel>
+            I started my adventure with programming at the age of 12. Since then I have learned a lot and written thousands of lines of code. At this point, I have closed a few commercial projects and I am working on my portfolio.
+          </Accordion.Panel>
         </Accordion.Item>
 
         <Accordion.Item className={classes.item} value="credit-card">
-          <Accordion.Control>Do you store credit card information securely?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
+          <Accordion.Control>
+            How much does a website cost?
+          </Accordion.Control>
+          <Accordion.Panel>
+            It all depends on the size of the project. Simple landing pages starts from 100$, large business pages can take up 1000$. In addition to the cost of developing a website, you must be prepared to spend about $50-100 per year for hosting and domain name (we will help you choose the right one for your needs).
+          </Accordion.Panel>
         </Accordion.Item>
 
         <Accordion.Item className={classes.item} value="payment">
-          <Accordion.Control>What payment systems to you work with?</Accordion.Control>
-          <Accordion.Panel>{placeholder}</Accordion.Panel>
+          <Accordion.Control>
+            How much does it take to make a website?
+          </Accordion.Control>
+          <Accordion.Panel>
+            It all depends on the size of the project. Simple landing pages take a few days to a week, large business pages can take up to a month.
+          </Accordion.Panel>
         </Accordion.Item>
       </Accordion>
     </Container>
