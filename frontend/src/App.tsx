2beba29d38115512c0bde639f7d2891536733c9d
jeli-t <jelitotomek@gmail.com>
2023-07-12 15:25:56 +0200

    add Privacy page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { Footer } from './components/Footer';
 import HomePage from './pages/HomePage';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Privacy from './pages/Privacy';
 import UnderConstructionPage from './pages/UnderConstructionPage';
 import NotFoundPage from './pages/NotFoundPage';
 
@@ -35,6 +36,7 @@ export default function App() {
                             <Route path='/about' element={<About />} />
                             <Route path='/contact' element={<Contact />} />
                             <Route path='/get_started' element={<UnderConstructionPage />} />
+                            <Route path='/privacy' element={<Privacy />} />
                             <Route path='*' element={<NotFoundPage />} />
                         </Routes>
                     </AppShell>
