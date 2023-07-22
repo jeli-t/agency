e64feb7e5b72b0537c7072656002a258ec27d902
jeli-t <jelitotomek@gmail.com>
2023-07-23 00:43:17 +0200

    add Survey model and SurveySerializer

--- backend/get_started/models.py
+++ backend/get_started/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+    businessType = models.CharField(max_length=100, null=True, blank=True)
+    industry = models.CharField(max_length=100, null=True, blank=True)
+    mainGoal = models.CharField(max_length=100, null=True, blank=True)
+    website = models.CharField(max_length=200, null=True, blank=True)
+    socialMedia = models.CharField(max_length=200, null=True, blank=True)
+    advertising = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.email
