ec87d3eb0f58e4db52ad80229a5b9dcea031f023
jeli-t <jelitotomek@gmail.com>
2023-07-23 00:31:53 +0200

    install rest_framework

--- backend/get_started/views.py
+++ backend/get_started/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
 
+@api_view(['POST'])
+@parser_classes([JSONParser])
 def submitSurvey(request):
-    print('survey submited')
+    return Response('Submited')
