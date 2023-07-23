6b49125d8577f3129c0460e4120611129c4a41c2
jeli-t <jelitotomek@gmail.com>
2023-07-23 11:56:24 +0200

    add SurveyAdmin to django admin panel

--- backend/get_started/admin.py
+++ backend/get_started/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Survey
 
-admin.site.register(Survey)
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_submitted', 'report',)
+
+admin.site.register(Survey, SurveyAdmin)
