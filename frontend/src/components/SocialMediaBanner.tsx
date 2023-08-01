c4df50c69f89ace509cc7271335aba51312a9506
jeli-t <jelitotomek@gmail.com>
2023-08-01 10:31:16 +0200

    update SocialMediaBanner

--- frontend/src/components/SocialMediaBanner.tsx
+++ frontend/src/components/SocialMediaBanner.tsx
@@ -1,86 +1,86 @@
-import { createStyles, Title, Text, Button, Container, rem } from '@mantine/core';
+import { createStyles, Title, Text, Container, rem } from '@mantine/core';
 import { Dots } from '../assets/Dots';
 
+
 const useStyles = createStyles((theme) => ({
-  wrapper: {
-    position: 'relative',
-    paddingTop: rem(120),
-    paddingBottom: rem(80),
+    wrapper: {
+        position: 'relative',
+        marginTop: rem(80),
+        marginBottom: rem(80),
 
-    [theme.fn.smallerThan('sm')]: {
-      paddingTop: rem(80),
-      paddingBottom: rem(60),
+        [theme.fn.smallerThan('sm')]: {
+            marginTop: rem(60),
+            marginBottom: rem(60),
+        },
     },
-  },
 
-  inner: {
-    position: 'relative',
-    zIndex: 1,
-  },
-
-  dots: {
-    position: 'absolute',
-    color: theme.colorScheme === 'dark' ? theme.colors.gray[6] : theme.colors.gray[3],
-
-    [theme.fn.smallerThan('sm')]: {
-      display: 'none',
+    inner: {
+        position: 'relative',
+        zIndex: 1,
     },
-  },
 
-  dotsLeft: {
-    left: 0,
-    top: 0,
-  },
+    dots: {
+        position: 'absolute',
+        color: theme.colorScheme === 'dark' ? theme.colors.gray[6] : theme.colors.gray[3],
 
-  title: {
-    textAlign: 'center',
-    fontWeight: 800,
-    fontSize: rem(40),
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    marginBottom: theme.spacing.xs,
-    fontFamily: `Roboto, ${theme.fontFamily}`,
-
-    [theme.fn.smallerThan('xs')]: {
-      fontSize: rem(28),
-      textAlign: 'left',
+        [theme.fn.smallerThan('sm')]: {
+            display: 'none',
+        },
     },
-  },
 
-  description: {
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    fontFamily: `Roboto, ${theme.fontFamily}`,
-    fontSize: rem(24),
-    fontWeight: 500,
-    textAlign: 'center',
-
-    [theme.fn.smallerThan('xs')]: {
-      fontSize: rem(20),
-      textAlign: 'left',
+    dotsLeft: {
+        left: 0,
+        top: 0,
     },
-  },
+
+    title: {
+        textAlign: 'left',
+        fontWeight: 800,
+        fontSize: rem(40),
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        marginBottom: theme.spacing.xs,
+        fontFamily: `Roboto, ${theme.fontFamily}`,
+
+        [theme.fn.smallerThan('xs')]: {
+            fontSize: rem(28),
+            textAlign: 'left',
+        },
+    },
+
+    description: {
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        fontFamily: `Roboto, ${theme.fontFamily}`,
+        fontSize: rem(24),
+        fontWeight: 500,
+        textAlign: 'left',
+
+        [theme.fn.smallerThan('xs')]: {
+            fontSize: rem(20),
+            textAlign: 'left',
+        },
+    },
 }));
 
 export function SocialMediaBanner() {
-  const { classes } = useStyles();
+    const { classes } = useStyles();
 
-  return (
-    <Container className={classes.wrapper} size={1400}>
-      <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
-      <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
-      <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
-      <Dots className={classes.dots} style={{ right: 0, top: 60 }} />
+    return (
+        <Container className={classes.wrapper} size={1400}>
+            <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
+            <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
+            <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
+            <Dots className={classes.dots} style={{ right: 0, top: 60 }} />
 
-      <div className={classes.inner}>
-        <Title className={classes.title}>
-            Social media
-        </Title>
-
-        <Container p={0} size={700}>
-          <Text className={classes.description}>
-            On this page you will find all the information about how we can help you manage social media and build a community around your brand.
-          </Text>
+            <div className={classes.inner}>
+                <Container p={0} size={800}>
+                    <Title order={1} className={classes.title}>
+                        The power of Social media
+                    </Title>
+                    <Text className={classes.description}>
+                        The power of social media is undeniable and has had a profound impact on various aspects of society, communication, and business. Social media platforms have billions of daily active users, providing companies with the ability to reach a wide and diverse audience. By creating and sharing content on social media channels, you can connect with your target audiences, increase brand visibility, drive traffic to your website and achieve specific marketing goals. Don't miss this opportunity.
+                    </Text>
+                </Container>
+            </div>
         </Container>
-      </div>
-    </Container>
-  );
+    );
 }
