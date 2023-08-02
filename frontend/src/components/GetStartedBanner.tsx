5a80b182a26462d13976474000bcc02b3ed04311
jeli-t <jelitotomek@gmail.com>
2023-08-02 13:56:50 +0200

    update GetStartedBanner

--- frontend/src/components/GetStartedBanner.tsx
+++ frontend/src/components/GetStartedBanner.tsx
@@ -1,99 +1,85 @@
-import { createStyles, Title, Text, Button, Container, rem } from '@mantine/core';
+import { createStyles, Title, Text, Container, rem } from '@mantine/core';
 import { Dots } from '../assets/Dots';
 
+
 const useStyles = createStyles((theme) => ({
-  wrapper: {
-    position: 'relative',
-    paddingTop: rem(120),
-    paddingBottom: rem(80),
+    wrapper: {
+        position: 'relative',
+        marginTop: rem(80),
+        marginBottom: rem(80),
 
-    [theme.fn.smallerThan('sm')]: {
-      paddingTop: rem(80),
-      paddingBottom: rem(60),
+        [theme.fn.smallerThan('sm')]: {
+            marginTop: rem(60),
+            marginBottom: rem(60),
+        },
     },
-  },
 
-  inner: {
-    position: 'relative',
-    zIndex: 1,
-  },
-
-  dots: {
-    position: 'absolute',
-    color: theme.colorScheme === 'dark' ? theme.colors.gray[6] : theme.colors.gray[3],
-
-    [theme.fn.smallerThan('sm')]: {
-      display: 'none',
+    inner: {
+        position: 'relative',
+        zIndex: 1,
     },
-  },
 
-  dotsLeft: {
-    left: 0,
-    top: 0,
-  },
+    dots: {
+        position: 'absolute',
+        color: theme.colorScheme === 'dark' ? theme.colors.gray[6] : theme.colors.gray[3],
 
-  title: {
-    textAlign: 'center',
-    fontWeight: 800,
-    fontSize: rem(40),
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    marginBottom: theme.spacing.xs,
-    fontFamily: `Roboto, ${theme.fontFamily}`,
-
-    [theme.fn.smallerThan('xs')]: {
-      fontSize: rem(28),
-      textAlign: 'left',
+        [theme.fn.smallerThan('sm')]: {
+            display: 'none',
+        },
     },
-  },
 
-  description: {
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    fontFamily: `Roboto, ${theme.fontFamily}`,
-    fontSize: rem(24),
-    fontWeight: 500,
-    textAlign: 'center',
-
-    [theme.fn.smallerThan('xs')]: {
-      fontSize: rem(20),
-      textAlign: 'left',
+    dotsLeft: {
+        left: 0,
+        top: 0,
     },
-  },
 
-  highlight: {
-    position: 'relative',
-    backgroundColor: theme.primaryColor,
-    borderRadius: theme.radius.sm,
-    padding: `${rem(4)} ${rem(12)}`,
-  },
+    title: {
+        textAlign: 'left',
+        fontWeight: 800,
+        fontSize: rem(40),
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        marginBottom: theme.spacing.xs,
+        fontFamily: `Roboto, ${theme.fontFamily}`,
+
+        [theme.fn.smallerThan('xs')]: {
+            fontSize: rem(28),
+            textAlign: 'left',
+        },
+    },
+
+    description: {
+        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        fontFamily: `Roboto, ${theme.fontFamily}`,
+        fontSize: rem(24),
+        fontWeight: 500,
+        textAlign: 'left',
+
+        [theme.fn.smallerThan('xs')]: {
+            fontSize: rem(20),
+            textAlign: 'left',
+        },
+    },
 }));
 
 export function GetStartedBanner() {
-  const { classes } = useStyles();
+    const { classes } = useStyles();
 
-  return (
-    <Container className={classes.wrapper} size={1400}>
-      <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
-      <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
-      <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
-      <Dots className={classes.dots} style={{ right: 0, top: 60 }} />
+    return (
+        <Container className={classes.wrapper} size={1400}>
+            <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
+            <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
+            <Dots className={classes.dots} style={{ right: 0, top: 60 }} />
 
-      <div className={classes.inner}>
-        <Title className={classes.title}>
-          Not sure where to start?
-        </Title>
-
-        <Container p={0} size={800}>
-          <Text className={classes.description}>
-            Answer a few questions below for personalized suggestions.
-          </Text>
-          <Text className={classes.description}>
-            The report is <span style={{fontWeight: '700'}}>100% free</span>, and we will prepare it within 24 hours.
-          </Text>
-          <Text className={classes.description}>
-            The survey should take no more than 5 minutes.
-          </Text>
+            <div className={classes.inner}>
+                <Container p={0} size={800}>
+                    <Title order={1} className={classes.title}>
+                      Not sure where to start?
+                    </Title>
+                    <Text className={classes.description}>
+                    Answer a few questions below. We will prepare a <span style={{fontWeight: 700}}>free</span> personalized strategy and offer for your business. The survey should take no more than 5 minutes. It is non-committal and we will prepare a report within 24 hours.
+                    </Text>
+                </Container>
+            </div>
         </Container>
-      </div>
-    </Container>
-  );
+    );
 }
