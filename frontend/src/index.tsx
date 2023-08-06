170c037fd282ac029ca19e1a36aadf266ad1b492
jeli-t <jelitotomek@gmail.com>
2023-08-06 22:04:57 +0200

    delete loading text

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <Suspense fallback="loading">
+      <Suspense>
         <App />
       </Suspense>
     </HelmetProvider>
