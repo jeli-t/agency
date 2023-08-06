5cb2c96dadeb74682b9c1e41cd6a8e8e4958ffe8
jeli-t <jelitotomek@gmail.com>
2023-08-06 10:55:18 +0200

    translate DigitalMarketingStrategy

--- frontend/src/components/DigitalMarketingStrategy.tsx
+++ frontend/src/components/DigitalMarketingStrategy.tsx
@@ -1,5 +1,6 @@
 import { createStyles, useMantineTheme, Title, rem, Text } from '@mantine/core';
 import question_mark from './../assets/question_mark.svg'
+import { useTranslation } from 'react-i18next';
 
 
 const useStyles = createStyles((theme) => ({
@@ -113,6 +114,7 @@ const useStyles = createStyles((theme) => ({
 export function DigitalMarketingStrategy() {
     const theme = useMantineTheme();
     const { classes } = useStyles();
+    const { t } = useTranslation();
 
     return (
         <div className={classes.wrapper}>
@@ -122,10 +124,10 @@ export function DigitalMarketingStrategy() {
                 </div>
                 <div className={classes.description}>
                     <Title order={2} className={classes.title}>
-                        The best strategy?
+                        {t("digital_marketing_strategy.title")}
                     </Title>
                     <Text className={classes.text}>
-                        You may ask what is the best digital marketing strategy, and there is no one right answer. Selection of the channels depends on the business's goals, target audience, budget, and the nature of products or services being promoted. See how we build successful strategies step by step.
+                        {t("digital_marketing_strategy.text")}
                     </Text>
                 </div>
             </div>
