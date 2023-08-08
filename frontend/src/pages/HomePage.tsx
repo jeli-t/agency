a38d15d4ff0cd9577e9043f0dd4a6baffecaa35f
jeli-t <jelitotomek@gmail.com>
2023-08-08 23:43:23 +0200

    add Open Graph meta tags

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
         <>
             <Helmet>
                 <title>Jeli Digital Agency | Websites | SEO | Social media</title>
-                <meta name='description' content='We will help you become visible online. From the website to social media. We provide comprehensive and tailored solutions for small and medium-sized businesses.' />
+                <meta name='description' content='Comprehensive and tailored digital marketing for businesses. Profesional Websites, Search Engine Optimization, Social media' />
                 <link rel='canonical' href='/' />
             </Helmet>
             <MantineProvider theme={mainTheme} withGlobalStyles withNormalizeCSS>
