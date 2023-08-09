34322abaeb76f308680cff75f6d92d8c598e7915
jeli-t <jelitotomek@gmail.com>
2023-08-09 17:54:08 +0200

    update and translate FaqSection

--- frontend/src/components/FaqSection.tsx
+++ frontend/src/components/FaqSection.tsx
@@ -1,4 +1,29 @@
 import { Container, Title, Accordion, createStyles, rem } from '@mantine/core';
+import { useTranslation } from 'react-i18next';
+
+
+export const MOCKDATA = [
+  {
+      question: 'faq_section.question1',
+      answer: "faq_section.answer1",
+  },
+  {
+    question: 'faq_section.question2',
+    answer: "faq_section.answer2",
+  },
+  {
+    question: 'faq_section.question3',
+    answer: "faq_section.answer3",
+  },
+  {
+    question: 'faq_section.question4',
+    answer: "faq_section.answer4",
+  },
+  {
+    question: 'faq_section.question5',
+    answer: "faq_section.answer5",
+  },
+];
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
@@ -22,59 +47,40 @@ const useStyles = createStyles((theme) => ({
 }));
 
 
+interface QuestionProps {
+  question: React.ReactNode;
+  answer: React.ReactNode;
+}
+
+export function Question({ question, answer }: QuestionProps) {
+  const { classes } = useStyles();
+  const { t } = useTranslation();
+
+  return (
+    <Accordion.Item className={classes.item} value={t(`${question}`)}>
+      <Accordion.Control>
+        {t(`${question}`)}
+      </Accordion.Control>
+      <Accordion.Panel>
+        {t(`${answer}`)}
+      </Accordion.Panel>
+    </Accordion.Item>
+  );
+}
+
 export function FaqSection() {
   const { classes } = useStyles();
+  const { t } = useTranslation();
+
+  const questions = MOCKDATA.map((feature, index) => <Question {...feature} key={index} />);
+
   return (
     <Container className={classes.wrapper}>
       <Title order={3} align="center" className={classes.title}>
-        Frequently Asked Questions
+        {t("faq_section.title")}
       </Title>
-
       <Accordion variant="separated">
-        <Accordion.Item className={classes.item} value="reset-password">
-          <Accordion.Control>
-            Is your company registered?
-          </Accordion.Control>
-          <Accordion.Panel>
-            Not yet but I'am working on that. For now I work as a freelancer based on a contract for work.
-          </Accordion.Panel>
-        </Accordion.Item>
-
-        <Accordion.Item className={classes.item} value="another-account">
-          <Accordion.Control>
-            What technologies do you use?
-          </Accordion.Control>
-          <Accordion.Panel>
-            We create most of the websites using Wordpress Elementor, so anyone can easily update information on the site without hiring a specialist. For more complex projects, we use React JS and Django.
-          </Accordion.Panel>
-        </Accordion.Item>
-
-        <Accordion.Item className={classes.item} value="newsletter">
-          <Accordion.Control>
-            What experience do you have?
-          </Accordion.Control>
-          <Accordion.Panel>
-            I started my adventure with programming at the age of 12. Since then I have learned a lot and written thousands of lines of code. At this point, I have closed a few commercial projects and I am working on my portfolio.
-          </Accordion.Panel>
-        </Accordion.Item>
-
-        <Accordion.Item className={classes.item} value="credit-card">
-          <Accordion.Control>
-            How much does a website cost?
-          </Accordion.Control>
-          <Accordion.Panel>
-            It all depends on the size of the project. Simple landing pages starts from 100$, large business pages can take up 1000$. In addition to the cost of developing a website, you must be prepared to spend about $50-100 per year for hosting and domain name (we will help you choose the right one for your needs).
-          </Accordion.Panel>
-        </Accordion.Item>
-
-        <Accordion.Item className={classes.item} value="payment">
-          <Accordion.Control>
-            How much does it take to make a website?
-          </Accordion.Control>
-          <Accordion.Panel>
-            It all depends on the size of the project. Simple landing pages take a few days to a week, large business pages can take up to a month.
-          </Accordion.Panel>
-        </Accordion.Item>
+        {questions}
       </Accordion>
     </Container>
   );
