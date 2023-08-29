53301101fd6cbf88d50d0ce24314861b60ea86c4
jeli-t <jelitotomek@gmail.com>
2023-08-29 22:09:51 +0200

    fix GoodWebsite design

--- frontend/src/components/GoodWebsite.tsx
+++ frontend/src/components/GoodWebsite.tsx
@@ -41,6 +41,8 @@ const useStyles = createStyles((theme) => ({
         fontFamily: `Roboto, ${theme.fontFamily}`,
         fontSize: rem(24),
         fontWeight: 600,
+        marginTop: 10,
+        marginBottom: 10,
 
         [theme.fn.smallerThan('lg')]: {
             fontSize: rem(20),
