579bf451bc8641dd7bb12f4d8bc53d9a014e611e
jeli-t <jelitotomek@gmail.com>
2023-09-06 23:11:45 +0200

    add ColorScheme and MantineProvider config

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,27 +1,48 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import dynamic from 'next/dynamic'
+import { MantineProvider, ColorSchemeProvider, ColorScheme, AppShell } from '@mantine/core';
+import { BrowserRouter } from 'react-router-dom';
+import CookieConsent from "react-cookie-consent";
+import { useLocalStorage } from '@mantine/hooks';
+import ScrollToTop from '../components/ScrollToTop';
+import { HeaderMenu } from '../components/HeaderMenu';
+import { Footer } from '../components/Footer';
+import { useTranslation } from 'react-i18next';
+
 
 export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
+    const { Component, pageProps } = props;
 
-  return (
-    <>
-      <Head>
-        <title>Page title</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
+    const { t } = useTranslation();
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'dark',
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </>
-  );
+    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+        key: 'jeli-color-scheme',
+        defaultValue: 'dark',
+        getInitialValueInEffect: true,
+    });
+
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+    return (
+        <>
+            <Head>
+                <title>Jeli Digital Agency</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+            </Head>
+
+            <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+                <MantineProvider withGlobalStyles withNormalizeCSS theme={{primaryColor: 'orange', colorScheme}}>
+                    {/* <CookieConsent style={{paddingLeft: 20, paddingRight: 20}} buttonText={t("cookie_consent.button")}>{t("cookie_consent.text1")}<a href={t("cookie_consent.privacy_link")}>{t("cookie_consent.link_text")}</a> {t("cookie_consent.text2")}</CookieConsent> */}
+                    {/* <BrowserRouter> */}
+                        {/* <ScrollToTop></ScrollToTop> */}
+                        {/* <AppShell header={<HeaderMenu />} footer={<Footer />} padding={0}> */}
+                            <Component {...pageProps} />
+                        {/* </AppShell> */}
+                    {/* </BrowserRouter> */}
+                </MantineProvider>
+            </ColorSchemeProvider>
+        </>
+    );
 }
