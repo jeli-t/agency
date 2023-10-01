edd60f0921c78ed6def277f27089d77adea86a96
jeli-t <jelitotomek@gmail.com>
2023-10-01 20:56:41 +0200

    add 404 page

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,79 @@
+import Head from 'next/head'
+import { HeaderMenu } from '../components/HeaderMenu';
+import { Footer } from '../components/Footer';
+import { createStyles, Title, Text, Button, Container, Group, rem } from '@mantine/core';
+import Link from "next/link";
+import { useTranslation } from 'next-i18next';
+
+
+const useStyles = createStyles((theme) => ({
+    root: {
+        paddingTop: rem(200),
+        paddingBottom: rem(200),
+    },
+
+    label: {
+        textAlign: 'center',
+        fontWeight: 900,
+        fontSize: rem(220),
+        lineHeight: 1,
+        marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
+
+        [theme.fn.smallerThan('sm')]: {
+        fontSize: rem(120),
+        },
+    },
+
+    title: {
+        fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+        textAlign: 'center',
+        fontWeight: 900,
+        fontSize: rem(38),
+
+        [theme.fn.smallerThan('sm')]: {
+        fontSize: rem(32),
+        },
+    },
+
+    description: {
+        maxWidth: rem(500),
+        margin: 'auto',
+        marginTop: theme.spacing.xl,
+        marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
+    },
+}));
+
+export default function NotFoundPage() {
+    const { classes } = useStyles();
+    const { t } = useTranslation();
+
+
+    return (
+        <>
+            <Head>
+                <title>Jeli Digital Agency | Websites | SEO | Social media</title>
+                <meta name='description' content='404' />
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="robots" content="Noindex" />
+            </Head>
+            <HeaderMenu />
+            <Container className={classes.root}>
+                <div className={classes.label}>404</div>
+                <Title className={classes.title}>You have found a secret place.</Title>
+                <Text color="dimmed" size="lg" align="center" className={classes.description}>
+                Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has
+                been moved to another URL.
+                </Text>
+                <Group position="center">
+                <Link href='/'>
+                    <Button color='orange' variant="light" size="md">
+                    Take me back to home page
+                    </Button>
+                </Link>
+                </Group>
+            </Container>
+            <Footer />
+        </>
+  );
+}
